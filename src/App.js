305a8b93cf89cd8fdf10cfcc7a0238cb6d14a4bc
Gerald Bailey <gbailey@snakeheadsoftware.com>
2017-02-12 09:29:11 -0600

    Set up initial react plugins I'll likely use in this project

--- src/App.js
+++ src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>YouTube React Demo</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        // components will go here.
       </div>
     );
   }
---
+++ src/config/config.js
@@ -0,0 +1,2 @@
+const API_KEY = 'AIzaSyAvrApIE7IpTvMDcRTZXm2Ggn4l9mrO9X4';
+module.exports = API_KEY;
